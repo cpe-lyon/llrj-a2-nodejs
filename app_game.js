d9cec6e5be2351b35750218f1eb960f153f75e1c
Loan <aubryloan74@gmail.com>
2024-11-20 09:02:30 +0100

    Separated app.js for chat and game application

---
+++ app_game.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const Stomp = require("stomp-client");
+const room = require('./utils/room');
+const login = require('./utils/login');
+Object.assign(global, { WebSocket: require('ws') });
+let rooms = {};
+
+const app = express();
+app.use(express.json());
+const gameRoutes = require('./routes/game');
+app.use('/game', gameRoutes);
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:8088',
+    methods: ['GET', 'POST']
+  }
+});
+
+
+// Connection to ActiveMQ
+// stompClient.activate();
+
+server.listen(6666, () => {
+  console.log('Server listening on port 6666');
+});
